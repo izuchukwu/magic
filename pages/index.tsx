867f00c90f2e0c20f18cad2533ab7bbe65e5a6ff
Izuchukwu Elechi <izuchukwu@hey.com>
2022-09-12 11:27:51 -0700

    Adds GA and prompt chaining

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,107 +1,18 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {
-	ActionIcon,
-	Autocomplete,
-	Box,
-	Group,
-	JsonInput,
-	Loader,
-	Paper,
-	Space,
-	Stack,
-	Text,
-	Textarea,
-	Timeline
-} from '@mantine/core'
-import {
-	IconBrandTwitter,
-	IconChevronDown,
-	IconChevronRight,
-	IconPencil,
-	IconPlus
-} from '@tabler/icons'
+import {Space, Stack, Text} from '@mantine/core'
 import {EmojiFavi} from '../src/components/EmojiFavi'
-import {usePrompt} from '../src/hooks/everyprompt'
-import React, {useCallback, useEffect, useState} from 'react'
-import {Tiptap} from '../src/components/tiptap/Tiptap'
-import {decode} from 'html-entities'
-
-const promptForFormat = (prompt: string, format: string) => {
-	const promptWithoutFinalColon = prompt.endsWith(':')
-		? prompt.slice(0, -1)
-		: prompt
-	switch (format) {
-		case 'Article':
-			return `${promptWithoutFinalColon}\nas an essay in HTML format with headers, sections, subtopics, and bulleted lists:`
-		case 'Answer':
-			return `${promptWithoutFinalColon}:`
-		case 'Tweet Thread':
-			return `${promptWithoutFinalColon}\nas a viral thread of tweets prefixed by their number in the series (for example, "1/" for the first tweet):`
-		case 'JSON':
-			return `${promptWithoutFinalColon}\nin perfect valid JSON format:`
-		case '':
-			return prompt
-		default:
-			return `${promptWithoutFinalColon}\nas ${format}:`
-	}
-}
+import React, {useState} from 'react'
+import {MagicPrompt} from '../src/magic/MagicPrompt'
+import _ from 'lodash'
+import {nanoid} from 'nanoid'
+import Script from 'next/script'
 
 const Home: NextPage = () => {
-	const {completion, duration, error, isLoading, runWithVariables} =
-		usePrompt('magic')
-	const [prompt, setPrompt] = useState<string>()
-	const [format, setFormat] = useState('Answer')
-	const [result, setResult] = useState<string>()
-
-	/** Updates prompt while typing */
-	const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-		setPrompt(e.currentTarget.value)
-	}
-
-	/** Asks EP the prompt */
-	const onAsk = useCallback(() => {
-		if (!prompt) return
-		runWithVariables({prompt: promptForFormat(prompt, format)})
-	}, [prompt, format, runWithVariables])
-
-	/** Calls onAsk on Return */
-	const onReturn = useCallback(
-		(event: React.KeyboardEvent) => {
-			// If Return is not held, return early
-			if (event.key !== 'Enter') return
-
-			// If there is no prompt, do not ask EP
-			if (!prompt) {
-				event.preventDefault()
-				return
-			}
-
-			// If Shift is held, add a new line
-			// If not, ask EP
-			if (!event.shiftKey) {
-				event.preventDefault()
-				onAsk()
-			}
-		},
-		[onAsk, prompt]
-	)
-
-	const [lastFormat, setLastFormat] = useState(format)
-	/** Calls onAsk and clears the completion when prompt format is changed */
-	useEffect(() => {
-		if (format === lastFormat) return
-		setLastFormat(format)
-		setResult('')
-		onAsk()
-	}, [onAsk, format, lastFormat])
-
-	/** Sets result when completion returns */
-	useEffect(() => {
-		const decodedCompletion = decode(completion)
-		setResult(decodedCompletion)
-	}, [completion])
+	const [prompts, setPrompts] = useState<
+		{promptID: string; completion?: string}[]
+	>([{promptID: nanoid()}])
 
 	return (
 		<div className={styles.container}>
@@ -113,6 +24,17 @@ const Home: NextPage = () => {
 				/>
 				<EmojiFavi emoji="🧙" />
 			</Head>
+			<Script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-Z0VGQP574V"
+			></Script>
+			<Script id="google-">
+				{`window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'G-Z0VGQP574V');`}
+			</Script>
 
 			<Stack
 				sx={{
@@ -129,127 +51,38 @@ const Home: NextPage = () => {
 						sx={{
 							fontSize: 99,
 							textAlign: 'center',
-							zIndex: 999,
 							isolation: 'isolate',
-							['-webkit-transform']: 'translate3d(0,0,0)',
-							['-webkit-perspective']: '1000',
-							['-webkit-backface-visibility']: 'hidden',
-							['-webkit-transform-style']: 'preserve-3d'
+							WebkitTransform: 'translate3d(0,0,0)',
+							WebkitPerspective: '1000',
+							WebkitBackfaceVisibility: 'hidden',
+							WebkitTransformStyle: 'preserve-3d'
 						}}
 					>
 						🧙
 					</Text>
-					<Paper
-						shadow="sm"
-						withBorder
-						sx={{
-							borderColor: 'rgba(0, 0, 0, 0.16)',
-							borderRadius: 13,
-							padding: '5px 15px',
-							isolation: 'isolate',
-							zIndex: 999
-						}}
-						mt={-27}
-						pl={15}
-						pr={10}
-					>
-						<Group sx={{gap: 10}} align="top">
-							<Box mt={13}>
-								<IconChevronRight
-									size={20}
-									color="rgba(0, 0, 0, 0.5)"
-								/>
-							</Box>
-							<Textarea
-								autoFocus
-								autosize={true}
-								size="lg"
-								sx={{width: 600}}
-								placeholder="Ask Magic Anything"
-								variant="unstyled"
-								onKeyDown={onReturn}
-								onChange={onChange}
-								value={prompt}
-							/>
-							<Box mt={13} pr={5}>
-								<Loader
-									size={20}
-									color="green"
-									sx={{
-										visibility: isLoading
-											? 'visible'
-											: 'hidden'
-									}}
-								/>
-							</Box>
-						</Group>
-						<Group sx={{width: '100%'}} pb={5} position="right">
-							<Autocomplete
-								data={[
-									'Answer',
-									'Article',
-									'Tweet Thread',
-									'JSON'
-								]}
-								filter={() => true}
-								value={format}
-								variant="filled"
-								radius="md"
-								size="xs"
-								sx={{width: 150, cursor: 'pointer'}}
-								onClick={(e) => e.currentTarget.select()}
-								rightSection={
-									<IconChevronDown size={15} opacity={0.5} />
-								}
-								styles={{
-									rightSection: {pointerEvents: 'none'}
-								}}
-								icon={<IconPencil size={15} />}
-								mt={10}
-								onChange={setFormat}
-							/>
-						</Group>
-					</Paper>
+					{prompts.map((prompt, i) => (
+						<MagicPrompt
+							index={i}
+							key={prompt.promptID}
+							onAddPrompt={() => {
+								prompts.splice(i + 1, 0, {promptID: nanoid()})
+								setPrompts([...prompts])
+							}}
+							onCompletionSelect={(completion) => {
+								prompts[i].completion = completion
+								setPrompts([...prompts])
+							}}
+							lastCompletion={
+								i > 0 ? prompts[i - 1].completion : undefined
+							}
+						/>
+					))}
 				</Stack>
-				{result && (
-					<Box sx={{width: 600, position: 'relative'}} mt={15}>
-						{format === 'Article' && <Tiptap content={result} />}
-						{format === 'Tweet Thread' && (
-							<Timeline bulletSize={24} lineWidth={2}>
-								{result
-									.trim()
-									.split('\n')
-									.map((tweet, i) => (
-										<Timeline.Item
-											bullet={
-												<IconBrandTwitter size={12} />
-											}
-											key={i}
-										>
-											<Text>{tweet}</Text>
-										</Timeline.Item>
-									))}
-							</Timeline>
-						)}
-						{format === 'JSON' && (
-							<JsonInput
-								value={result}
-								minRows={4}
-								variant="unstyled"
-								autosize
-							/>
-						)}
-						{!['Article', 'Tweet Thread', 'JSON'].includes(
-							format
-						) && <Text>{result}</Text>}
-					</Box>
-				)}
+
 				<Space sx={{height: '5vh'}} />
 			</Stack>
 		</div>
 	)
 }
-
-const PromptField = () => {}
 
 export default Home
--- src/magic/MagicPrompt.tsx
+++ src/magic/MagicPrompt.tsx
@@ -1,3 +1,256 @@
-type MagicPromptProps = {}
+import {
+	ActionIcon,
+	Autocomplete,
+	Box,
+	Group,
+	JsonInput,
+	Loader,
+	Paper,
+	Stack,
+	Textarea,
+	Timeline,
+	Text
+} from '@mantine/core'
+import {
+	IconChevronRight,
+	IconChevronDown,
+	IconPencil,
+	IconPlus,
+	IconBrandTwitter
+} from '@tabler/icons'
+import {decode} from 'html-entities'
+import {useState, useCallback, useEffect} from 'react'
+import {Tiptap} from '../components/tiptap/Tiptap'
+import {usePrompt} from '../hooks/everyprompt'
 
-export const MagicPrompt = ({}: MagicPromptProps) => {}
+type MagicPromptProps = {
+	index?: number
+	lastCompletion?: string
+
+	onAddPrompt?: () => void
+	onCompletionSelect?: (completion: string) => void
+}
+
+export const MagicPrompt = ({
+	index = 0,
+	lastCompletion,
+	onAddPrompt,
+	onCompletionSelect
+}: MagicPromptProps) => {
+	const {completion, duration, error, isLoading, runWithVariables} =
+		usePrompt('magic')
+	const [prompt, setPrompt] = useState<string>()
+	const [format, setFormat] = useState('Answer')
+	const [result, setResult] = useState<string>()
+
+	/** Updates prompt while typing */
+	const onChange = useCallback(
+		(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+			setPrompt(e.currentTarget.value)
+		},
+		[]
+	)
+
+	/** Asks EP the prompt */
+	const onAsk = useCallback(() => {
+		if (!prompt) return
+		runWithVariables({
+			prompt: promptForFormat(prompt, format, lastCompletion)
+		})
+	}, [prompt, runWithVariables, format, lastCompletion])
+
+	/** Calls onAsk on Return */
+	const onReturn = useCallback(
+		(event: React.KeyboardEvent) => {
+			// If Return is not held, return early
+			if (event.key !== 'Enter') return
+
+			// If there is no prompt, do not ask EP
+			if (!prompt) {
+				event.preventDefault()
+				return
+			}
+
+			// If Shift is held, add a new line
+			// If not, ask EP
+			if (!event.shiftKey) {
+				event.preventDefault()
+				onAsk()
+			}
+		},
+		[onAsk, prompt]
+	)
+
+	const [lastFormat, setLastFormat] = useState(format)
+	/** Calls onAsk and clears the completion when prompt format is changed */
+	useEffect(() => {
+		if (format === lastFormat) return
+		setLastFormat(format)
+		setResult('')
+		onAsk()
+	}, [onAsk, format, lastFormat])
+
+	/** Sets result when completion returns */
+	useEffect(() => {
+		const decodedCompletion = decode(completion)
+		setResult(decodedCompletion)
+
+		// Set selected completion
+		onCompletionSelect?.(decodedCompletion)
+	}, [completion, onCompletionSelect])
+
+	return (
+		<Stack justify="center" align="center">
+			<Paper
+				shadow="sm"
+				withBorder
+				sx={{
+					borderColor: 'rgba(0, 0, 0, 0.16)',
+					borderRadius: 13,
+					padding: '5px 15px',
+					isolation: 'isolate',
+					zIndex: 99
+				}}
+				pl={15}
+				pr={10}
+			>
+				<Group sx={{gap: 10}} align="top">
+					<Box mt={13}>
+						<IconChevronRight
+							size={20}
+							color="rgba(0, 0, 0, 0.5)"
+						/>
+					</Box>
+					<Textarea
+						autoFocus
+						autosize={true}
+						size="lg"
+						sx={{width: 600}}
+						placeholder={
+							index
+								? 'Ask for changes to the result, like "summarize this" or "expand on the first section"'
+								: 'Ask Magic Anything'
+						}
+						variant="unstyled"
+						onKeyDown={onReturn}
+						onChange={onChange}
+						value={prompt}
+					/>
+					<Box mt={13} pr={5}>
+						<Loader
+							size={20}
+							color="green"
+							sx={{
+								visibility: isLoading ? 'visible' : 'hidden'
+							}}
+						/>
+					</Box>
+				</Group>
+				<Group sx={{width: '100%'}} pb={5} position="right">
+					<Autocomplete
+						data={['Answer', 'Article', 'Tweet Thread', 'JSON']}
+						filter={() => true}
+						value={format}
+						variant="filled"
+						radius="md"
+						size="xs"
+						sx={{width: 150, cursor: 'pointer'}}
+						onClick={(e) => e.currentTarget.select()}
+						rightSection={
+							<IconChevronDown size={15} opacity={0.5} />
+						}
+						styles={{
+							rightSection: {pointerEvents: 'none'}
+						}}
+						icon={<IconPencil size={15} />}
+						mt={10}
+						onChange={setFormat}
+						disabled={isLoading}
+					/>
+				</Group>
+			</Paper>
+			{result && (
+				<Box sx={{width: 600, position: 'relative'}} mt={15}>
+					{format === 'Article' && <Tiptap content={result} />}
+					{format === 'Tweet Thread' && (
+						<Timeline bulletSize={24} lineWidth={2}>
+							{result
+								.trim()
+								.split('\n')
+								.map((tweet, i) => (
+									<Timeline.Item
+										bullet={<IconBrandTwitter size={12} />}
+										key={i}
+									>
+										<Text>{tweet}</Text>
+									</Timeline.Item>
+								))}
+						</Timeline>
+					)}
+					{format === 'JSON' && (
+						<JsonInput
+							value={result}
+							minRows={4}
+							variant="unstyled"
+							autosize
+						/>
+					)}
+					{!['Article', 'Tweet Thread', 'JSON'].includes(format) && (
+						<Text>{result}</Text>
+					)}
+				</Box>
+			)}
+			<ActionIcon
+				mt={0}
+				mb={15}
+				variant="filled"
+				radius="xl"
+				size="sm"
+				color="gray"
+				className="showOnParentHover"
+				sx={{
+					zIndex: 199,
+					marginTop: 20,
+					['--parent-hover-opacity']: 0.65
+				}}
+				onClick={onAddPrompt}
+			>
+				<IconPlus />
+			</ActionIcon>
+		</Stack>
+	)
+}
+
+/* -- Helpers -- */
+
+const promptForFormat = (
+	prompt: string,
+	format: string,
+	lastCompletion?: string
+) => {
+	// Add last completion
+	const promptWithLastCompletion = lastCompletion
+		? `Given the following content:\n${lastCompletion}\n\n${prompt}`
+		: prompt
+
+	// Remove final semicolon if present
+	const promptWithoutFinalColon = promptWithLastCompletion.endsWith(':')
+		? promptWithLastCompletion.slice(0, -1)
+		: promptWithLastCompletion
+
+	// Format
+	switch (format) {
+		case 'Article':
+			return `${promptWithoutFinalColon}\nas an engaging in-depth article in HTML format with headers, sections on the most major topics, its subtopics, and bulleted lists, starting with an <h1> tag:`
+		case 'Answer':
+			return `${promptWithoutFinalColon}:`
+		case 'Tweet Thread':
+			return `${promptWithoutFinalColon}\nas a viral thread of tweets prefixed by their number in the series (for example, "1/" for the first tweet):`
+		case 'JSON':
+			return `${promptWithoutFinalColon}\nin perfect valid JSON format:`
+		case '':
+			return promptWithLastCompletion
+		default:
+			return `${promptWithoutFinalColon}\nas ${format}:`
+	}
+}
